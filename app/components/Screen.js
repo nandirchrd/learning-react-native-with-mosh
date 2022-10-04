3e19c4afa6f256e853ef023aa492ce43171d5046
nandirchrd <nandirchrd@gmail.com>
2022-10-04 11:33:00 +0700

    Extracting the screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const Screen = ({ children, style }) => {
+	return (
+		<SafeAreaView style={[style, styles.screen]}>{children}</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		paddingTop: Constants.statusBarHeight,
+	},
+});
+export default Screen;
